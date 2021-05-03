75b3e72464115a351e90a7cd57380c8b64d145d2
davedwight <dpdwight@gmail.com>
2021-05-03 16:35:56 -0600

    Finished mvp problems

--- js/index.js
+++ js/index.js
@@ -47,8 +47,90 @@ let logoImage = document.getElementById("logo-img");
 logoImage.setAttribute("src", siteContent["nav"]["img-src"]);
 
 let ctaImage = document.querySelector("#cta-img");
-ctaImage.src = "img/header-img.png";
-console.log("ctaImage: ", ctaImage);
+ctaImage.src = siteContent.cta["img-src"];
 
 let middleImage = document.querySelector("#middle-img");
-middleImage.src = "img/mid-page-accent.jpg";
+middleImage.src = siteContent["main-content"]["middle-img-src"];
+
+// Updating the textContent of the links
+let links = document.querySelectorAll("a");
+let linksArray = Array.from(links);
+
+linksArray[0].textContent = siteContent.nav["nav-item-1"];
+linksArray[1].textContent = siteContent.nav["nav-item-2"];
+linksArray[2].textContent = siteContent.nav["nav-item-3"];
+linksArray[3].textContent = siteContent.nav["nav-item-4"];
+linksArray[4].textContent = siteContent.nav["nav-item-5"];
+linksArray[5].textContent = siteContent.nav["nav-item-6"];
+
+const newLink1 = document.createElement("a");
+newLink1.textContent = "Last Link";
+document.querySelector("nav").appendChild(newLink1);
+
+const newLink2 = document.createElement("a");
+newLink2.textContent = "First Link";
+document.querySelector("nav").prepend(newLink2);
+
+// Updating the h1 in the cta section
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.textContent = siteContent.cta.h1;
+
+// Updating the button text
+let buttonText = document.querySelector("button");
+buttonText.textContent = siteContent.cta.button;
+
+// Updating the main content
+// Updating the top headers
+let topTextHeader = document.querySelectorAll(".top-content .text-content h4");
+let topTextHeaderArray = Array.from(topTextHeader);
+
+topTextHeaderArray[0].textContent = siteContent["main-content"]["features-h4"];
+topTextHeaderArray[1].textContent = siteContent["main-content"]["about-h4"];
+
+// Updating the top paragraphs
+let topTextContent = document.querySelectorAll(".top-content .text-content p");
+
+topTextContent[0].textContent = siteContent["main-content"]["features-content"];
+topTextContent[1].textContent = siteContent["main-content"]["features-content"];
+
+// Updating the bottom headers
+let bottomTextHeader = document.querySelectorAll(
+  ".bottom-content .text-content h4"
+);
+
+bottomTextHeader[0].textContent = siteContent["main-content"]["services-h4"];
+bottomTextHeader[1].textContent = siteContent["main-content"]["product-h4"];
+bottomTextHeader[2].textContent = siteContent["main-content"]["vision-h4"];
+
+// Updating the bottom paragraphs
+let bottomTextContent = document.querySelectorAll(
+  ".bottom-content .text-content p"
+);
+
+bottomTextContent[0].textContent =
+  siteContent["main-content"]["services-content"];
+bottomTextContent[1].textContent =
+  siteContent["main-content"]["product-content"];
+bottomTextContent[2].textContent =
+  siteContent["main-content"]["vision-content"];
+
+// Updating the contact section
+let contactTitle = document.querySelector(".contact h4");
+contactTitle.textContent = siteContent["contact"]["contact-h4"];
+
+let contactContent = document.querySelectorAll(".contact p");
+contactContent[0].textContent = siteContent["contact"]["address"];
+contactContent[1].textContent = siteContent["contact"]["phone"];
+contactContent[2].textContent = siteContent["contact"]["email"];
+
+// Updating the footer section
+let footer = document.querySelector("footer p");
+footer.textContent = siteContent.footer.copyright;
+
+// Changing the color of the nav text
+linksArray[0].style.color = "green";
+linksArray[1].style.color = "green";
+linksArray[2].style.color = "green";
+linksArray[3].style.color = "green";
+linksArray[4].style.color = "green";
+linksArray[5].style.color = "green";
